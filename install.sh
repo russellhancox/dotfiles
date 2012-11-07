cbd923fa98e2c74d6e036b1e22da4eac9c6084a0
Russell Hancox <russell@hancox.us>
2012-11-07 13:16:26 +0100

    Smarter install process

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 hash git >/dev/null 2>&1 || (echo "Please install Git" && exit 0)
 
 echo "Creating symlinks..."
-for f in `ls .dotfiles | grep -v install.sh | grep -v README.md`; do 
-  ln -s ${HOME}/.dotfiles/${f} ~/.${f} >/dev/null 2>&1; 
+EXCLUDE="install.sh README.md solarized_dark.terminal"
+LS=`ls .dotfiles`
+for x in ${EXCLUDE}; do
+  LS=`echo ${LS} | grep -v ${x}`;
+done
+for f in `${LS}`; do 
+  ln -s ${HOME}/.dotfiles/${f} ${HOME}/.${f} >/dev/null 2>&1;
 done
 
 if [[ ! -f "${HOME}/vim/bundle/vundle/README.md" ]]; then
@@ -11,7 +16,7 @@ if [[ ! -f "${HOME}/vim/bundle/vundle/README.md" ]]; then
   rm -fr ${HOME}/.vim/bundle/vundle/* ${HOME}/.vim/bundle/vundle/.*
   git clone https://github.com/gmarik/vundle.git ${HOME}/.vim/bundle/vundle
   echo "Installing other bundles"
-  vim -N -u ~/.vim/config/bundles.vim +BundleInstall +quitall
+  vim -N -u ${HOME}/.vim/config/bundles.vim +BundleInstall +quitall
   echo "Building Command-T bundle"
   pushd .
   cd ${HOME}/.vim/bundle/Command-T/ruby/command-t
