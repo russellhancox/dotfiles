40709ee39f054becdeb9b2503371df5f8a1840e0
Russell Hancox <russell@hancox.us>
2012-11-07 12:15:17 +0000

    Always update VIM bundles, update submodules during install

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 [[ ${PWD} != ${HOME} ]] && (echo "Must be run from ${HOME}" && exit 0)
 hash git >/dev/null 2>&1 || (echo "Please install Git" && exit 0)
 
+echo "Updating submodules"
+git submodule init
+git submodule update
+
 echo "Creating symlinks..."
 for f in `ls .dotfiles | grep -v install.sh | grep -v README.md`; do 
   ln -s ${HOME}/.dotfiles/${f} ~/.${f} >/dev/null 2>&1; 
 done
 
-if [[ ! -f "${HOME}/vim/bundle/vundle/README.md" ]]; then
-  echo "Installing latest vundle bundle"
-  rm -fr ${HOME}/.vim/bundle/vundle/* ${HOME}/.vim/bundle/vundle/.* 2>/dev/null
-  git clone https://github.com/gmarik/vundle.git ${HOME}/.vim/bundle/vundle
-  echo "Installing other bundles"
-  vim -N -u ~/.vim/config/bundles.vim +BundleInstall +quitall
-  echo "Building Command-T bundle"
-  pushd .
-  cd ${HOME}/.vim/bundle/Command-T/ruby/command-t
-  ruby extconf.rb
-  make
-  popd
-else
-  echo "Skipping vundle download"
-fi
+echo "Installing latest vundle bundle"
+rm -fr ${HOME}/.vim/bundle/vundle/* ${HOME}/.vim/bundle/vundle/.* 2>/dev/null
+git clone https://github.com/gmarik/vundle.git ${HOME}/.vim/bundle/vundle
+echo "Installing other vim bundles"
+vim -N -u ~/.vim/config/bundles.vim +BundleInstall +quitall
+echo "Building Command-T bundle"
+pushd .
+cd ${HOME}/.vim/bundle/Command-T/ruby/command-t
+ruby extconf.rb
+make
+popd
 
 # If running on a Mac, set-up Mac defaults..
 if uname | grep -q Darwin; then
