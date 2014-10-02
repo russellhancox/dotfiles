872ba4baea8ecc153030b1c438713884c8cab2c4
Russell Hancox <russell@hancox.us>
2014-10-02 12:54:32 -0400

    Update dock orientation in osx_defaults.sh

--- osx_defaults.sh
+++ osx_defaults.sh
@@ -122,8 +122,9 @@ defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool NO
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool NO
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool NO
 
-# Automatically hide and show the Dock
+# Automatically hide and show the Dock and be on the right hand side.
 defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock orientation -string 'right';
 
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
