99c7b56fc14db2a4440d13250a5ed32e3ede4a94
Russell Hancox <russell@hancox.us>
2019-06-04 12:38:08 -0400

    Remove old window arrangements, add 2 for youtube. Remove grid hotkey

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -51,12 +51,6 @@ windowHotkey:bind({}, 'f', function()
   end
 end)
 
--- Show grid
-windowHotkey:bind({}, 'g', function()
-  windowHotkey:exit()
-  hs.grid.show()
-end)
-
 -- Helper to bind a key in W-Mode to a grid setting
 local function makeWindowHotkey(hotKey, gridTable)
   windowHotkey:bind({}, hotKey, function()
@@ -71,7 +65,7 @@ end
 makeWindowHotkey('l', {x=0,  y=0,  w=25, h=50}) -- Left half
 makeWindowHotkey('r', {x=25, y=0,  w=25, h=50}) -- Right half
 makeWindowHotkey('0', {x=0,  y=0,  w=29, h=47}) -- Side-by-side 100 col diff
-makeWindowHotkey('4', {x=30, y=0,  w=40, h=50}) -- Right 40% width
-makeWindowHotkey('6', {x=0,  y=0,  w=60, h=50}) -- Left 60% width
-makeWindowHotkey('e', {x=0,  y=25, w=25, h=25}) -- Bottom left corner
-makeWindowHotkey('k', {x=0,  y=0,  w=25, h=47}) -- Left 95% height
+
+makeWindowHotkey('y', {x=25,  y=30, w=25, h=20}) -- Bottom right corner
+makeWindowHotkey('u', {x=25,  y=0, w=25, h=35}) -- Upper right corner
+
