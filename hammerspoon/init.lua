b3a9f503a1546394fd148246d8341640090c5636
Russell Hancox <russell@hancox.us>
2024-03-12 18:14:13 -0400

    hammerspoon: Re-add MicMute spoon (with light customization)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,3 +12,7 @@ require 'windows'
 -- Keys
 recbind = hs.loadSpoon('RecursiveBinder')
 require 'keys'
+
+-- MicMute
+micmute = hs.loadSpoon('MicMute')
+micmute:bindHotkeys({toggle={'', 'F19'}}, 0.1)
