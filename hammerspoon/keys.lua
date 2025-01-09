69c4c5546d4f75a499364a2875299573cfb54de2
Russell Hancox <russell@hancox.us>
2025-01-09 12:32:29 -0500

    hs: Add menu option to switch default output audio device

--- hammerspoon/keys.lua
+++ hammerspoon/keys.lua
@@ -5,22 +5,51 @@ local function appLaunchFn(name)
   return f
 end
 
-local singleKey = recbind.singleKey
+-- Helper to change the default audio output device and then
+-- show a notification if successful.
+local function switchOutputDevice(name)
+  local dev = hs.audiodevice.findOutputByName(name)
+  if dev then
+    success = dev:setDefaultOutputDevice()
+    if success then
+      local notif = hs.notify.new(nil, {
+        title='Changed output audio device',
+        subTitle=name,
+        autoWithdraw=true,
+      })
+      notif:send()
+    end
+  end
+end
 
--- Change the RecursiveBind styling to use Roboto Condensed and appear in the
+-- Change the RecursiveBind styling to use Avenir Next Condensed and appear in the
 -- middle of the screen instead of the bottom edge.
 recbind.helperFormat.textFont = 'Avenir Next Condensed'
+recbind.helperEntryLengthInChar = 25
 recbind.helperFormat.atScreenEdge = 0
 
-recursiveMenu = recbind.recursiveBind({
+local singleKey = recbind.singleKey
+
+-- Returns another bound menu for each output audio device, allowing to switch
+-- device with a menu key.
+local function outputDeviceTable()
+  keyArray = {}
+  for i, dev in ipairs(hs.audiodevice.allOutputDevices()) do
+    keyArray[singleKey(tostring(i), dev:name())] = function() switchOutputDevice(dev:name()) end
+  end
+  return recbind.recursiveBind(keyArray)
+end
+
+hs.hotkey.bind({}, 'F10', recbind.recursiveBind({
   [singleKey('space', 'hints')] = hs.hints.windowHints,
   [singleKey('w', 'win')] = function() windowHotkey:enter() end,
   [singleKey('c', 'caf')] = function() caf:clicked() end,
   [singleKey('l', 'lock')] = appLaunchFn('ScreenSaverEngine.app'),
-  [singleKey('s', 'spotify+')] = {
+  [singleKey('s', 'sound+')] = {
       [singleKey('p', 'Play/Pause')] = hs.spotify.playpause,
       [singleKey('n', 'Next')] = hs.spotify.next,
       [singleKey('b', 'Back')] = hs.spotify.previous,
+      [singleKey('s', 'Switch output')] = outputDeviceTable(),
       [singleKey('d', 'Display')] = function()
           local artist = hs.spotify.getCurrentArtist()
           local track = hs.spotify.getCurrentTrack()
@@ -48,5 +77,5 @@ recursiveMenu = recbind.recursiveBind({
       [singleKey('c', 'Console')] = hs.openConsole,
       [singleKey('r', 'Reload config')] = hs.reload,
   },
-})
-hs.hotkey.bind({}, 'F10', recursiveMenu)
+}))
+
